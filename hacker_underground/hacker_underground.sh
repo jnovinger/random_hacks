485b17435fece8fac64d5501dbf62964f17cd39c
Jason Novinger <jnovinger@gmail.com>
2011-03-04 12:24:38 -0600

    - Didn't survive new puzzle as of Fri, 04 Mar 2011 12:17:31 +0600

--- hacker_underground/hacker_underground.sh
+++ hacker_underground/hacker_underground.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ## Inspired by (stolen from) http://news.ycombinator.com/item?id=2288604
 ## Python versions does the same thing, but is a lot more verbose
+
+# didn't survive puzzle as of Fri, 04 Mar 2011 12:17:31 +0600
+
 cat puzzle.txt | tr ',' '\n' | sort -n | uniq -d
