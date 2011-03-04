3d96799fa3ddb74a52fee862f9fb86ee55ef2225
Jason Novinger <jnovinger@gmail.com>
2011-03-04 13:03:03 -0600

    - Changed to meet puzzle as of Puzzle has changed at Fri, 04 Mar 2011 12:17:31 +0600

--- hacker_underground/hacker_underground.py
+++ hacker_underground/hacker_underground.py
@@ -1,15 +1,45 @@
+#!/usr/bin/python
+
+# A quick one-off to solve the problem posed at http://hackerunderground.com/
+
+import re
+from math import sqrt, floor
+
+p = re.compile('[a-z]+', re.IGNORECASE)
+
 file = open('puzzle.txt')
+prime_candidates = []
 
 while 1:
     line = file.readline()
     if not line:
         break
-    lines = line.split(', ')
+    lines = line.split(' ')
     lines.sort()
     prev_line = 0
     for line in lines:
         if line == prev_line:
-            print line
-            break
+            if p.match(line) == None:
+                prime_candidates.append(line)
         else:
             prev_line = line
+    break
+   
+primes = []      
+for candidate in prime_candidates:
+    # rough cut off
+    limit = int(sqrt(float(candidate))) + 1
+    candidate = int(candidate)
+    is_prime = True
+    
+    for i in range (2, limit):
+        if is_prime:
+            if candidate % i == 0:
+                is_prime = False
+                break
+        else:
+            break
+    
+    if is_prime:
+        print candidate
+        break
