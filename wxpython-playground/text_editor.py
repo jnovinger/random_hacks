28ab74743f57bb661172e11e7f686a5187971142
Jason Novinger <jnovinger@gmail.com>
2011-07-23 14:46:56 -0500

    Adds some inactive menu items.

--- wxpython-playground/text_editor.py
+++ wxpython-playground/text_editor.py
@@ -8,6 +8,18 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title = title, size = (200, 100))
         self.control = wx.TextCtrl(self, style = wx.TE_MULTILINE)
+        self.CreateStatusBar()
+
+        filemenu = wx.Menu()
+
+        filemenu.Append(wx.ID_ABOUT, '&About', 'Information about this program.')
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, '&Exit', 'Quit')
+
+        menubar = wx.MenuBar()
+        menubar.Append(filemenu, '&File')
+        self.SetMenuBar(menubar)
+
         self.Show(True)
 
 app = wx.App(False)
