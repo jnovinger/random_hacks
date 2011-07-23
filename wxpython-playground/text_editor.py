2af8caa0e4e87ca4a8f57e13296b500ac61d1deb
Jason Novinger <jnovinger@gmail.com>
2011-07-23 14:53:23 -0500

    Adds functionality to menu items.

--- wxpython-playground/text_editor.py
+++ wxpython-playground/text_editor.py
@@ -5,6 +5,7 @@ import wx
 class MyFrame(wx.Frame):
     """Derive new class of Frame."""
 
+    #noinspection PyArgumentList
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title = title, size = (200, 100))
         self.control = wx.TextCtrl(self, style = wx.TE_MULTILINE)
@@ -12,15 +13,26 @@ class MyFrame(wx.Frame):
 
         filemenu = wx.Menu()
 
-        filemenu.Append(wx.ID_ABOUT, '&About', 'Information about this program.')
+        menuAbout = filemenu.Append(wx.ID_ABOUT, '&About', 'Information about this program.')
         filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT, '&Exit', 'Quit')
+        menuExit = filemenu.Append(wx.ID_EXIT, '&Exit', 'Quit')
+
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
         menubar = wx.MenuBar()
         menubar.Append(filemenu, '&File')
         self.SetMenuBar(menubar)
 
         self.Show(True)
+
+    def OnAbout(self, event):
+        dlg = wx.MessageDialog(self, 'A small text editor', 'About Small Text Editor', wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self, event):
+        self.Close(True)
 
 app = wx.App(False)
 frame = MyFrame(None, 'Small Editor')
